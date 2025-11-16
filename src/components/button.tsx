ed650e427dd649c2c896334293e84f37f47918a7
setdek123 <rodriguezalan785@gmail.com>
2025-11-16 17:14:15 -0300

    Corrigindo props erro do button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,26 +1,23 @@
 'use client';
 
-import { FormEvent } from "react";
-
-
-
 interface Props {
     text: string,
     size: string,
-    onclickt: ()=> void
+    onclickt?: (e: any) => void;
 }
 
-const Button = ({text, size, onclickt}: Props) => {
+const Button = ({ text, size, onclickt }: Props) => {
 
     return (
         <div>
-            <button className={`${size} bg-green-500 rounded-md shadow hover:bg-green-700 cursor-pointer border-b-gray-900`}
-            onClick={onclickt}
-            > 
-                <h2 className="text-white  text-2xl font-extrabold">{text}</h2>
+            <button
+                className={`${size} bg-green-500 rounded-md shadow hover:bg-green-700 cursor-pointer border-b-gray-900`}
+                onClick={onclickt}
+            >
+                <h2 className="text-white text-2xl font-extrabold">{text}</h2>
             </button>
         </div>
-    )
+    );
 };
 
 export default Button;
